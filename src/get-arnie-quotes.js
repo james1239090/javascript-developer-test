033031ba316de1c89315f13cb27c00759351f0a0
James <james1239090@gmail.com>
2021-01-11 23:42:14 +1100

    finished getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
+const handleResult = (result) => {
+  const { body, status } = result;
+  const { message } = JSON.parse(body);
+  const key = status === 200 ? 'Arnie Quote' : 'FAILURE';
+
+  return {
+    [key]: message,
+  };
+};
+
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
+  const requests = urls.map(httpGet);
+  const results = await Promise.all(requests);
+
+  return results.map(handleResult);
 };
 
 module.exports = {
