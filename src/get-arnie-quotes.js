1d386d4d88e20eabec6721d1348d0e3a1cb08271
James <james1239090@example.com>
2026-05-21 11:57:50 +1000

    Implement getArnieQuotes response mapping

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,11 +1,21 @@
 const { httpGet } = require('./mock-http-interface');
 
+const formatResponse = ({ status, body }) => {
+  const { message } = JSON.parse(body);
+
+  return status === 200
+    ? { 'Arnie Quote': message }
+    : { 'FAILURE': message };
+};
+
 /**
  * @param {GetArnieQuotesInput} urls
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  const results = await Promise.all(urls.map(httpGet));
+
+  return results.map(formatResponse);
 };
 
 module.exports = {
